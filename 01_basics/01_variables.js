5ad79280fc174c9e27e047cd22cf2528a3c96185
ruchit2607 <104899649+ruchit2607@users.noreply.github.com>
2024-03-19 06:27:16 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 692
+let accountEmail = "ruchitpatel0026@gmail.com"
+var accountPassword = "12345"
+accountCity = "ahmedabad"
+let accountState;
+
+// accountId = 341 // Not Allowed
+
+
+
+accountEmail = "raja1234@gmail.com"
+accountPassword = "888999"
+accountCity = "Jaipur"
+
+
+console.log(accountId);
+
+
+/* Prefer Not To Use Var 
+Because Of Issue In Block Scope 
+And Functional Scope */
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat All Js Code As Newer Version
+
+//  alert( 3 + 3 ) // we are using nodjs , not browser 
+
+console.log(3+3) 
+// code readability should be high
+console.log("ruchit")
+
+let name = "Ruchit"
+let age = 19
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Ruchit");
