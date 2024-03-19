49d1c64f1476eec82ea54a8ef0fa02aa834bfaa6
ruchit2607 <104899649+ruchit2607@users.noreply.github.com>
2024-03-19 12:22:34 +0000

    Dates In Js

---
+++ 01_basics/07_datesin_js.js
@@ -0,0 +1,27 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+// let myOwnDate = new Date(2024, 2, 22)
+// let myOwnDate = new Date(2024, 2, 22, 5, 24)
+// let myOwnDate = new Date("2024-03-24")
+let myOwnDate = new Date("03-19-2024")
+// console.log(myOwnDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myOwnDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
