d1abca325dc41c2bf56328f283604c09de48941e
ruchit2607 <104899649+ruchit2607@users.noreply.github.com>
2024-03-21 08:46:38 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,53 @@
+
+
+function sayMyName(){
+    console.log("R");
+    console.log("U");
+    console.log("C");
+    console.log("H");
+    console.log("I");
+    console.log("T");
+}
+
+// sayMyName()
+
+function addTwoNumber(number1, number2){
+    // console.log(number1 + number2);
+    return number1 + number2
+}
+
+const result = addTwoNumber(4,5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please Enter a Username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Ruchit"))
+console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){
+        return num1
+}
+
+console.log(calculateCartPrice(200, 400, 600, 800, 1000));
+
+const user = {
+     username: "Ruchit",
+     price: 499
+}
+
+function handleObject(anyobject){
+      console.log(`Username is ${anyobject.username} and price is ${anyobject.price}.`);
+}
+
+// handleObject(user)
+handleObject({
+    
+})
